5c6e8849753a1d9556c756a64e55e4af5829d431
Anadil <mdanadil32@gmail.com>
2026-07-05 21:48:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="text-4xl font-bold text-blue-600">LETS BUILD NETFLIX-GPT</h1>
+    </>
+  )
+}
+
+export default App
